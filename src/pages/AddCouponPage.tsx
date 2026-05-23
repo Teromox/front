8df1dd9f0d7ebbeb384009fd90926e0ed5d29be9
JANG SEONG JU <printhelloworld0702@gmail.com>
2026-05-24 00:59:48 +0900

    쿠폰 코드 입력창 추가 및 불필요한 파일 삭제

--- src/pages/AddCouponPage.tsx
+++ src/pages/AddCouponPage.tsx
@@ -4,8 +4,8 @@ import { OuterBox, RoundContainer, BodyDiv, TitleDiv } from './style/PageStyle';
 import { Label, WhiteInput, BlackButton, LockedWhiteInput } from '../components/style/CompStyle';
 import { ContainerInBox, InputContainer, InnerInputContainer, InputsContainer } from './style/CreateVmStyle';
 
+// Vm 생성 페이지와 유사해 컴포넌트 스타일을 최대한 재사용하였음.
 function AddCouponPage() {
-
     // 쿠폰 등록 버튼 클릭 이벤트
     // 나중에 input 얻기할때 참고할 것
     const [couponCode, setCouponCode] = useState('');
@@ -36,7 +36,7 @@ function AddCouponPage() {
                                 type="text" 
                                 placeholder='쿠폰 코드를 입력하세요.'
                                 id='couponCodeInput'
-                                onChange={(e) => setCouponCode(e.target.value)}
+                                onChange={(e) => setCouponCode(e.target.value)} // 번경 될 때 마다 usestate에 업데이트
                             />
                         </InputContainer>
                         <BlackButton onClick={AddBtnClick} style={{ width: '100%', margin: '0.3dvh 0 3dvh 0' }}>
